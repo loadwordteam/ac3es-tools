ffd2dd7d0d2e184d37ade0d244fb1244f33e1ab4
Gianluigi Cusimano <gianluigi@interconnectit.com>
2017-06-19 10:01:58 +0100

    updated copyright

--- helpers.py
+++ helpers.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
+#  This file is part of AC3ES Tools.
+#
+#  AC3ES Tools is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  AC3ES Tools is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with AC3ES Tools.  If not, see <http://www.gnu.org/licenses/>.
+
 
 def b2uint(number):
     """Simple function for convert the bytes in little edian to
